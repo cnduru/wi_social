e17a1be07b5b9aa0e1e69e71810fd5ffbb7cdebf
Dan Skøtt Petersen <dan.skoett.petersen@gmail.com>
2014-11-07 14:57:15 +0100

    .

--- wi_social/wi_social/sentiment.py
+++ wi_social/wi_social/sentiment.py
@@ -2,10 +2,16 @@ from happyfuntokenizing import Tokenizer
 from math import log
 
 vocabulary = set()
-num_rev_pos = 0
-num_rev_neg = 0
-voc_pos = {}
-voc_neg = {}
+num_rev_1 = 0
+num_rev_2 = 0
+num_rev_3 = 0
+num_rev_4 = 0
+num_rev_5 = 0
+voc_1 = {}
+voc_2 = {}
+voc_3 = {}
+voc_4 = {}
+voc_5 = {}
 
 class Review:
     t = Tokenizer()
@@ -22,21 +28,42 @@ class Review:
     def __count_sentiments(self):
         global num_rev_pos, num_rev_neg, voc_pos, voc_neg
         if self.score == 1:
-            num_rev_pos += 1
+            num_rev_1 += 1
             for word in self.text:
-                if word in voc_pos:
-                    voc_pos[word] += 1
+                if word in voc_1:
+                    voc_1[word] += 1
                 else:
-                    voc_pos[word] = 1
-        elif self.score == -1:
-            num_rev_neg += 1
+                    voc_1[word] = 1
+        elif self.score == 2:
+            num_rev_2 += 1
             for word in self.text:
-                if word in voc_neg:
-                    voc_neg[word] += 1
+                if word in voc_2:
+                    voc_2[word] += 1
                 else:
-                    voc_neg[word] = 1
+                    voc_2[word] = 1
+        elif self.score == 3:
+            num_rev_3 += 1
+            for word in self.text:
+                if word in voc_3:
+                    voc_3[word] += 1
+                else:
+                    voc_3[word] = 1
+        elif self.score == 4:
+            num_rev_4 += 1
+            for word in self.text:
+                if word in voc_4:
+                    voc_4[word] += 1
+                else:
+                    voc_4[word] = 1
+        elif self.score == 5:
+            num_rev_5 += 1
+            for word in self.text:
+                if word in voc_5:
+                    voc_5[word] += 1
+                else:
+                    voc_5[word] = 1
         else:
-            print('Error: Sentiment must be 1 or -1')
+            print('Error: Sentiment must be 1-5')
 
     def get_score(self):
         return self.score
@@ -95,13 +122,7 @@ def parse_reviews(start_line, end_line):
                 print('{0:.2%}'.format(1.0 / (end_line - start_line + 1) * (curline - start_line)), end='\r')
 
             if line[:14] == "review/score: ":
-                if int(line[14]) < 3: # only include non-neutral reviews
-                    score = -1
-                elif int(line[14]) > 3:
-                    score = 1
-                else:
-                    continue
-
+                score = int(line[14])
             if line[:16] == "review/summary: ":
                 text += line[16:] + '.'
             if line[:13] == "review/text: ":
@@ -122,33 +143,63 @@ def parse_reviews(start_line, end_line):
 # returns the probability of a list of reviews having a given sentiment
 # if you are lost, look in slide 29
 def prob_sentiment(sentiment):
-    global num_rev_pos, num_rev_neg
-    n = num_rev_pos + num_rev_neg
+    global num_rev_1, num_rev_2, num_rev_3, num_rev_4, num_rev_5
+    n = sum([num_rev_1, num_rev_2, num_rev_3, num_rev_4, num_rev_5])
 
     if sentiment == 1:
-        nc = num_rev_pos
-    elif sentiment == -1:
-        nc = num_rev_neg
+        nc = num_rev_1
+    elif sentiment == 2:
+        nc = num_rev_2
+    elif sentiment == 3:
+        nc = num_rev_3
+    elif sentiment == 4:
+        nc = num_rev_4
+    elif sentiment == 5:
+        nc = num_rev_5
+    else:
+        nc = 0
+        print("Error: prob_sentiment(sentiment) - sentiment must be 1-5")
 
-    return (nc + 1)/(n + 2) # 2 is the number of classes (true / false)
+    return (nc + 1)/(n + 5) # 2 is the number of classes (1, 2, 3, 4, 5)
 
 # returns probability that a certain word has a given sentiment
 # if you are lost, look in slide 29
 def prob_word_in_sentiment(word, sentiment):
-    global voc_pos, voc_neg, vocabulary
+    global voc_1, voc_2, voc_3, voc_4, voc_5, vocabulary
 
     if sentiment == 1:
         nc = num_rev_pos
         if word in voc_pos:
-            nxc = voc_pos[word]
+            nxc = voc_1[word]
         else:
             nxc = 0 
-    elif sentiment == -1:
+    elif sentiment == 2:
         nc = num_rev_neg
-        if word in voc_neg:
+        if word in voc_2:
             nxc = voc_neg[word]
         else:
             nxc = 0
+    elif sentiment == 3:
+        nc = num_rev_neg
+        if word in voc_3:
+            nxc = voc_neg[word]
+        else:
+            nxc = 0
+    elif sentiment == 4:
+        nc = num_rev_neg
+        if word in voc_4:
+            nxc = voc_neg[word]
+        else:
+            nxc = 0
+    elif sentiment == 5:
+        nc = num_rev_neg
+        if word in voc_5:
+            nxc = voc_neg[word]
+        else:
+            nxc = 0
+    else:
+        print("Error in prob_word_sentiment()!")
+        return 0
 
     return (nxc + 1) / (nc + len(vocabulary))
 
@@ -168,14 +219,24 @@ def scoreTest(review):
 #### this is where the fun stuff happens!
 
 # parse reviews
-to_be_reviewed = parse_reviews(3000100, 3000100 + 1000000)
+#to_be_reviewed = parse_reviews(3000100, 3000100 + 1000000)
+
+to_be_reviewed = []
+
+to_be_reviewed.append(Review(-1, "The WORST coffee !. The worst!!! it is just plan awful bitter and strong and you cannot taste the Hazel Nut flavor at all!!!!!  Do not buy this product save your money!!!!!"))
 
 vocabulary = set()
-num_rev_pos = 0
-num_rev_neg = 0
-voc_pos = {}
-voc_neg = {}
-review_list = parse_reviews(1, 100000)
+num_rev_1 = 0
+num_rev_2 = 0
+num_rev_3 = 0
+num_rev_4 = 0
+num_rev_5 = 0
+voc_1 = {}
+voc_2 = {}
+voc_3 = {}
+voc_4 = {}
+voc_5 = {}
+review_list = parse_reviews(1, 1000000)
 
 total_hits = 0
 cnt = 0;
